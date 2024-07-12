2c18d2e00d07b7268ddf32412355d9aec8c57d54
Karen Agustino <117533246+karenagustino@users.noreply.github.com>
2024-07-12 09:01:49 +0700

    Added a new events endpoint (#6)
    * add events endpoint
    
    * updated comments

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { authRouter } from "./auth";
+import { eventRouter } from "./event";
 
 export const apiRouter = Router()
 
 apiRouter.use("/auth", authRouter)
+apiRouter.use("/events", eventRouter)
+
+
 
 // other routes go here
