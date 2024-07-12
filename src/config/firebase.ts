279ff9f311cf0d8319cac66f69664ea6fdf2322a
Gordon Cheung <georgehwc@gmail.com>
2024-07-11 17:50:53 -0700

    update config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,12 +1,13 @@
 import admin from "firebase-admin";
 import serviceAccount from "../../.secret/pmc-portal-credential.json";
 import { getAuth } from "firebase-admin/auth";
+import { Firestore, getFirestore } from "firebase-admin/firestore";
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
 });
 
-const auth = getAuth()
-const db = admin.firestore();
+const auth = getAuth();
+const db: Firestore = getFirestore("pmc-portal-db");
 
-export { auth, db }
+export { auth, db };
