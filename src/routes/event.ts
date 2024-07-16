d4327f2e1cd8e18829b938746bb52c1767908eec
Gordon Cheung <georgehwc@gmail.com>
2024-07-16 16:35:39 -0700

    env origin debug

--- src/routes/event.ts
+++ src/routes/event.ts
@@ -1,22 +1,22 @@
 import { Router } from "express";
 import { getEvents, getEventById } from "../controllers/events/event";
 
-export const eventRouter = Router()
+export const eventRouter = Router();
 
-eventRouter.get('/', async (req, res) => {
-    try {
-        const events = await getEvents();
-        res.status(200).json(events);
-    } catch (error: any) {
-        res.status(500).json({ error: error.message });
-    }
+eventRouter.get("/", async (req, res) => {
+  try {
+    const events = await getEvents();
+    res.status(200).json(process.env.ORIGIN);
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-eventRouter.get('/:id', async (req, res) => {
-    try {
-        const eventByID = await getEventById(req.params.id);
-        res.status(200).json(eventByID);
-    } catch (error: any) {
-        res.status(500).json({ error: error.message });
-    }
+eventRouter.get("/:id", async (req, res) => {
+  try {
+    const eventByID = await getEventById(req.params.id);
+    res.status(200).json(eventByID);
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
 });
