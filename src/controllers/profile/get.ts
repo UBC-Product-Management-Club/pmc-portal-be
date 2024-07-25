fa1ed92c0a50bebe34053fcf2866dcf00aa7e331
Toby <toby.lau075@gmail.com>
2024-07-25 15:25:56 +0700

    refactored /profile endpoint

--- src/controllers/profile/get.ts
+++ src/controllers/profile/get.ts
@@ -1,23 +1,15 @@
-import { Request, Response } from "express";
 import { db } from "../../config/firebase";
 import { User } from "../../schema/User";
 
-const getProfile = async (req: Request, res: Response) => {
-    const uid: string  = req.params.id
-    try {
-        const docRef = db.collection("users").doc(uid)
-        const userRef = await docRef.get()
-        if (userRef.exists) {
-            const user = userRef.data() as User
-            return res.send(200).json({...user})
-        } else {
-            throw Error()
-        }
-    } catch (error) {
-        // console.log(error)
-        return res.status(500).json({
-            "message": "error retrieving profile"
-        })
+const getProfile = async (uid: string) => {
+    const docRef = db.collection("users").doc(uid)
+    const userRef = await docRef.get()
+    if (userRef.exists) {
+        const user = userRef.data() as User
+        return {...user}
+    } else {
+        // console.log("throw error")
+        throw Error("User doesn't exist")
     }
 }
 
--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -1,11 +1,21 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { getProfile } from "../controllers/profile/get";
 // import { editProfile } from "../controllers/profile/edit";
 
 export const profileRouter = Router()
 
 // Get profile information
-profileRouter.get("/:id", getProfile)
+profileRouter.get("/:id", async (req: Request, res: Response) => {
+    const uid: string  = req.params.id
+    try {
+        const user = await getProfile(uid)
+        return res.status(200).json({...user})
+    } catch (error) {
+        return res.status(400).json({
+            message: error
+        })
+    }
+})
 
 // Edit proflie
 // profileRouter.put("/:id/edit", editProfile)
