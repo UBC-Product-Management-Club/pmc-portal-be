9693160b3fb082a42b5bc70f492276673fe86d5d
Toby <toby.lau075@gmail.com>
2024-07-26 16:30:41 +0700

    added auth middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,13 +2,15 @@ import { Router } from "express";
 import { authRouter } from "./auth";
 import { eventRouter } from "./event";
 import { profileRouter } from "./profile";
+import { verifyIdToken } from "../middleware/auth";
 
 export const apiRouter = Router()
 
 apiRouter.use("/auth", authRouter)
-apiRouter.use("/events", eventRouter)
-apiRouter.use("/profile", profileRouter)
 
+// Protected
+apiRouter.use("/profile", verifyIdToken, profileRouter)
+apiRouter.use("/events", verifyIdToken, eventRouter)
 
 
 // other routes go here
