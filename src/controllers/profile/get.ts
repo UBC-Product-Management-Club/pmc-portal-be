609f003a5f2f52f8d7d6b4099c48ce4b6bda4de0
Toby <toby.lau075@gmail.com>
2024-07-26 16:12:49 +0700

    refactored /profile

--- src/controllers/profile/get.ts
+++ src/controllers/profile/get.ts
@@ -1,16 +1,11 @@
 import { db } from "../../config/firebase";
 import { User } from "../../schema/User";
+import { userDocument } from "../auth/types";
 
-const getProfile = async (uid: string) => {
+const getProfile = async (uid: string): Promise<userDocument | undefined> => {
     const docRef = db.collection("users").doc(uid)
     const userRef = await docRef.get()
-    if (userRef.exists) {
-        const user = userRef.data() as User
-        return {...user}
-    } else {
-        // console.log("throw error")
-        throw Error("User doesn't exist")
-    }
+    return userRef.data() as userDocument | undefined
 }
 
 export { getProfile }
--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -7,12 +7,15 @@ export const profileRouter = Router()
 // Get profile information
 profileRouter.get("/:id", async (req: Request, res: Response) => {
     const uid: string  = req.params.id
-    try {
-        const user = await getProfile(uid)
-        return res.status(200).json({...user})
-    } catch (error) {
-        return res.status(400).json({
-            message: error
+    const user = await getProfile(uid)
+    if (user) {
+        return res.status(200).json({
+            exists: true,
+            ...user
+        })
+    } else {
+        return res.status(404).json({
+            exists: false
         })
     }
 })
