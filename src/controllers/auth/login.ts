105692edfbd87d2961ecf27624a809f276041dcb
Toby <toby.lau075@gmail.com>
2024-07-26 16:12:15 +0700

    refactored login

--- src/controllers/auth/login.ts
+++ src/controllers/auth/login.ts
@@ -8,27 +8,27 @@ import { checkUserExists } from "./utils"
         - idToken: idToken returned from successful firebase authentication. Used to exchange for session cookie
     HandleLogin authenticates idToken and returns a sessionCookie
 */
-const handleLogin = async (userUID: string, idToken: string): Promise<loginResponse> => {
+const handleLogin = async (userUID: string, idToken: string): Promise<loginResponse | undefined> => {
     if (!userUID || !idToken) {
         throw Error("400: Bad request")
     }
-    // check if user exists. If yes, redirect to /dashboard and return session_id. If no, go to /onboarding
-    // if (await checkUserExists(userUID)) {
-        const expiresIn = 60 * 60 * 1000
-        try {
-            // exchanges idToken for session cookie
-            const sessionCookie = await auth.createSessionCookie(idToken, { expiresIn })
-            const options = { maxAge: expiresIn, httpOnly: true, secure: true}
-            return { sessionCookie: sessionCookie, options: options }
-            // res.cookie('session', sessionCookie, options)
-        } catch (error) {
-            // Failed session cookie creation
-            throw Error("500: Failed to create session cookie")
-        }
-    // } else {
-    //     // Non-existent user tried to call login endpoint,
-    //     throw Error("400: Unauthorized access")
-    // }
+
+    // If user doesn't exist, return undefined
+    if (!await checkUserExists(userUID)) {
+        return undefined
+    }
+
+    const expiresIn = 60 * 60 * 1000
+    try {
+        // exchanges idToken for session cookie
+        const sessionCookie = await auth.createSessionCookie(idToken, { expiresIn })
+        const options = { maxAge: expiresIn, httpOnly: true, secure: true}
+        return { sessionCookie: sessionCookie, options: options }
+        // res.cookie('session', sessionCookie, options)
+    } catch (error) {
+        // Failed session cookie creation
+        throw Error("500: Failed to create session cookie")
+    }
 }
 
 // Checks if the current userRef exists.
--- src/controllers/auth/register.ts
+++ src/controllers/auth/register.ts
@@ -5,7 +5,7 @@ import { checkUserExists } from "./utils";
 
 
 // Handles initial user onboarding and login.
-const handleOnboarding = async (creds: loginReqBody, userDoc: userDocument): Promise<loginResponse> => {
+const handleOnboarding = async (creds: loginReqBody, userDoc: userDocument): Promise<void> => {
     if (await checkUserExists(creds.userUID)) {
         throw Error("User already exists.")
     }
@@ -13,17 +13,9 @@ const handleOnboarding = async (creds: loginReqBody, userDoc: userDocument): Pro
     // Create a new document with given UID
     const docRef = db.collection("users").doc(creds.userUID)
     try {
-
-        // might throw unsupported doc error
         await docRef.set(userDoc)
-
-        // Log user in
-        const session = await handleLogin(creds.userUID, creds.idToken)
-        return session
     } catch (error) {
-        // An error occurred? Delete the document
-        console.log(error)
-        throw Error("User creation failed.")
+        throw Error("Error creating user")
     }
 }
 
