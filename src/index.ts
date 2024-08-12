8c55a34214000bcdd44e85c204f6562e301c15d9
Toby <toby.lau075@gmail.com>
2024-08-12 15:51:49 +0700

    added express.json() back

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ app.use(
 app.options(`${process.env.ORIGIN}`, cors());
 
 app.use(cookieParser());
-// app.use(express.json());
+app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
 
