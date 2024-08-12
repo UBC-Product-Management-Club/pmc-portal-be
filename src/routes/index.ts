db245b1c035d57d7534d9f056be16e1f6e2ccf7a
Toby <toby.lau075@gmail.com>
2024-08-12 15:53:22 +0700

    brought original routes back for development

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,11 +8,12 @@ import { verifyIdToken } from "../middleware/auth";
 export const apiRouter = Router()
 
 apiRouter.use("/auth", authRouter)
+apiRouter.use("/events", eventRouter)
 apiRouter.use("/profile", profileRouter)
+apiRouter.use("/attendee", attendeeRouter)
 
 // Protected
-apiRouter.use("/profile", verifyIdToken, profileRouter)
-apiRouter.use("/events", eventRouter)
+// apiRouter.use("/profile", verifyIdToken, profileRouter)
 
 
 // other routes go here
