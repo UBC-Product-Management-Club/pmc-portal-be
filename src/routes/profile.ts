53cc9e4636e0f8bf3b434f20a2b3a222fa5bd3c1
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2024-08-30 20:59:16 -0700

    GET user's events in backend

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from "express";
-import { getProfile } from "../controllers/profile/get";
+import {getProfile, getRegisteredEvents} from "../controllers/profile/get";
 // import { editProfile } from "../controllers/profile/edit";
 
 export const profileRouter = Router()
@@ -12,6 +12,22 @@ profileRouter.get("/:id", async (req: Request, res: Response) => {
         return res.status(200).json({
             exists: true,
             ...user
+        })
+    } else {
+        return res.status(404).json({
+            exists: false
+        })
+    }
+})
+
+// Takes member ID, returns list of event IDs that member registered for
+profileRouter.get("/:id/events", async (req: Request, res: Response) => {
+    const uid: string  = req.params.id
+    const events = await getRegisteredEvents(uid)
+    if (events) {
+        return res.status(200).json({
+            exists: true,
+            ...events
         })
     } else {
         return res.status(404).json({
