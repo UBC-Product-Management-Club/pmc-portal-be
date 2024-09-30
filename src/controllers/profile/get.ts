3c9fd694b7496dc68a137fe216aa0d0b74d584f8
Jen Taruno <99060936+jentaruno@users.noreply.github.com>
2024-09-30 11:50:16 -0700

    Firebase auth0 bridge (#20)
    * Add new fields to onboarding
    
    * Fix build errors
    
    * POST req to backend (doesn't work yet)
    
    * POST req to backend works for guests
    
    * Collect email, standardize year_level to year
    
    * Update attendees to attendee_Ids
    
    * GET user's events in backend
    
    * Frontend can fetch regged events
    
    * update required fields
    
    * Patch user metadata in auth0
    
    * New backend endpoints for migration
    
    ---------
    
    Co-authored-by: Gordon Cheung <georgehwc@gmail.com>

--- src/controllers/profile/get.ts
+++ src/controllers/profile/get.ts
@@ -1,5 +1,5 @@
-import { db } from "../../config/firebase";
-import { userDocument } from "../auth/types";
+import {db} from "../../config/firebase";
+import {userDocument} from "../auth/types";
 
 const getProfile = async (uid: string): Promise<userDocument | undefined> => {
     const docRef = db.collection("users").doc(uid)
@@ -7,11 +7,23 @@ const getProfile = async (uid: string): Promise<userDocument | undefined> => {
     return userRef.data() as userDocument | undefined
 }
 
-const getRegisteredEvents = async(uid: string): Promise<string[] | undefined> => {
+const getProfileByEmail = async (email: string): Promise<string | undefined> => {
+    const userCollection = db.collection("users");
+    const querySnapshot = await userCollection.where("email", "==", email).get();
+
+    if (querySnapshot.empty) {
+        return undefined;
+    }
+
+    const userDoc = querySnapshot.docs[0];
+    return userDoc.id;
+}
+
+const getRegisteredEvents = async (uid: string): Promise<string[] | undefined> => {
     const attendeesRef = db.collection('attendees');
     const snapshot = await attendeesRef.where('member_Id', '==', uid).get();
     const eventIds = snapshot.docs.map(doc => doc.data().event_Id);
     return eventIds;
 }
 
-export { getProfile, getRegisteredEvents }
+export {getProfile, getProfileByEmail, getRegisteredEvents}
