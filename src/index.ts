1859a76e354bf0b9134fe9c81d6e5e0121aac2be
Gordon Cheung <gordonhwc1@gmail.com>
2024-10-12 00:15:43 -0700

    fix cors error

--- src/index.ts
+++ src/index.ts
@@ -18,26 +18,21 @@ const app = express();
 //   })
 // );
 
-const allowedOrigins = process.env.ORIGIN ? process.env.ORIGIN.split(',') : '*';
+const allowedOrigins = [process.env.ORIGIN];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins === '*' || allowedOrigins.indexOf(origin) !== -1) {
-        console.log("running1")
-        callback(null, true);
-      } else {
-        console.log("running2")
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    optionsSuccessStatus: 200,
-    credentials: true,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "Content-Type,Authorization"
-  })
-);
+const corsOptions = {
+  origin: function (origin: any, callback: any) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
 
+app.use(cors(corsOptions))
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
