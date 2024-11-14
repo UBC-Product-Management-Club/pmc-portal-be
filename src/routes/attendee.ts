52d140874e62ed1aec6004176aa1a9ebdc638b16
Toby <toby.lau075@gmail.com>
2024-11-14 08:17:01 -0800

    removed /addAttendee endpoint

--- src/routes/attendee.ts
+++ src/routes/attendee.ts
@@ -1,7 +1,5 @@
 import { Router } from "express";
 import { getAttendeeById, addAttendee } from "../controllers/events/attendee";
-import { Attendee } from "../schema/Event";
-import { v4 as uuidv4 } from "uuid";
 
 export const attendeeRouter = Router();
 
@@ -22,52 +20,6 @@ attendeeRouter.get("/:eventId", async (req, res) => {
     }
     const attendee = await getAttendeeById(eventId);
     res.status(200).json(attendee);
-  } catch (error: any) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-attendeeRouter.post("/addAttendee", async (req, res) => {
-  const attendee_Id = uuidv4(); // might create a new field in the collection
-  const requiredFields = [
-    "is_member",
-    "event_Id",
-    "first_name",
-    "last_name",
-    "email",
-    "ubc_student",
-    "familiarity",
-    "found_out",
-    "dietary",
-  ];
-  // need to figure how to pass in event ID and member ID dynamically?
-  // so when users click register, it sends their current member ID (through session) and the event ID it clicked
-  if (
-    req.body["ubc_student"] == "yes" ||
-    req.body["ubc_student"] == "no, other uni"
-  ) {
-    requiredFields.push("year", "faculty", "major");
-  }
-
-  if (req.body["ubc_student"] == "yes") {
-    requiredFields.push("student_id");
-  }
-
-  for (const field of requiredFields) {
-    if (!req.body.hasOwnProperty(field)) {
-      console.error(`Missing required field: ${field}`);
-      return res
-        .status(400)
-        .json({ error: `Missing required field: ${field}` });
-    }
-  }
-  const newAttendee: Attendee = { attendee_Id, ...req.body };
-
-  try {
-    await addAttendee(newAttendee);
-    res.status(201).json({
-      message: `Attendee with ID ${attendee_Id} has been created successfully.`,
-    });
   } catch (error: any) {
     res.status(500).json({ error: error.message });
   }
