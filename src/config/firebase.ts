ba4ecd37c767dc7be9f74f6ab5dd0b80f48e7001
Gordon Cheung <gocheung@macbook-pro.lan>
2025-01-08 20:31:44 -0800

    update path

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -17,18 +17,14 @@ const auth = getAuth();
 const db: Firestore = getFirestore(process.env.FIRESTORE_DATABASE_ID!);
 const fs = require('fs');
 
-console.log("Current working directory:", process.cwd());
-console.log("Directory contents:", fs.readdirSync('/'));
-console.log(".secret contents:", fs.readdirSync('.secret').catch(() => 'Cannot read directory'));
-
 const storage = new Storage({
-  keyFilename: ".secret/pmc-portal-credential.json"
+  keyFilename: "/pmc-portal-credential.json"
 })
 
 console.log(process.env.STRIPE_SECRET)
 const stripe = new Stripe(process.env.STRIPE_SECRET!)
 
 // Debug the new path
-console.log("Checking if credential file exists:", fs.existsSync(".secret/pmc-portal-credential.json"));
+console.log("Checking if credential file exists:", fs.existsSync("/pmc-portal-credential.json"));
 
 export { auth, db, storage, stripe };
