32e9b6ed033626bea14981dfc148ad14c10d163f
Toby <toby.lau075@gmail.com>
2025-02-16 13:23:39 -0800

    Implement users endpoints for admin (#43)
    * added admin route
    
    * admin rout

--- src/controllers/auth/users.ts
+++ src/controllers/auth/users.ts
@@ -1,9 +1,10 @@
 import { db } from "../../config/firebase";
+import { UserRequiredFields } from "./types";
 
-export const getAllUsers = async () => {
+export const getAllUsers = async (): Promise<UserRequiredFields[]> => {
     try {
         const users = await db.collection("users").get();
-        return users.docs.map((doc) => doc.data());
+        return users.docs.map((doc) => ({...doc.data(), id: doc.id} as UserRequiredFields));
     } catch (error) {
         console.error("Error fetching users: ", error);
         throw error;
