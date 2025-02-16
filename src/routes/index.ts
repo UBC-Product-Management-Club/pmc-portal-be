77786272122be49122cfac302b3afb3da10e53d1
Toby <toby.lau075@gmail.com>
2025-02-16 13:23:39 -0800

    Implement users endpoints for admin (#43)
    * added admin route
    
    * admin rout

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,21 +3,16 @@ import {authRouter} from "./auth";
 import {eventRouter} from "./event";
 import {attendeeRouter} from "./attendee";
 import {profileRouter} from "./profile";
-// import { verifyIdToken } from "../middleware/auth";
 import {paymentRouter} from "./payments";
 import {eventFormRouter} from "./eventForm";
+import { adminRouter } from "./admin";
 
 export const apiRouter = Router()
 
+apiRouter.use("/admin", adminRouter)
 apiRouter.use("/auth", authRouter)
 apiRouter.use("/events", eventRouter)
 apiRouter.use("/profile", profileRouter)
 apiRouter.use("/attendee", attendeeRouter)
 apiRouter.use("/payments", paymentRouter)
 apiRouter.use("/eventForm", eventFormRouter)
-
-// Protected
-// apiRouter.use("/profile", verifyIdToken, profileRouter)
-
-
-// other routes go here
