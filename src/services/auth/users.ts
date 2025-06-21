a6eb382df17f5699710454ba6b803af69ac5818f
Kevin Lei <kevinleimc@gmail.com>
2025-06-21 16:23:58 -0700

    added supabase creds and get users from supabase

--- src/services/auth/users.ts
+++ src/services/auth/users.ts
@@ -1,10 +1,26 @@
 import { db } from "../../config/firebase";
+import {supabase} from "../../config/supabase";
 import { UserRequiredFields } from "./types";
 
-export const getAllUsers = async (): Promise<UserRequiredFields[]> => {
+export const getAllUsers = async () => {
     try {
         const users = await db.collection("users").get();
-        return users.docs.map((doc) => ({...doc.data(), id: doc.id} as UserRequiredFields));
+        return users.size;
+    } catch (error) {
+        console.error("Error fetching users: ", error);
+        throw error;
+    }
+}
+
+
+export const getAllSupabaseUsers = async (): Promise<UserRequiredFields[]> => {
+    try {
+        const {data, error} = await supabase.from('User').select();
+        if (error || !data) {
+            throw new Error('Failed to fetch users: ' + error?.message);
+        }
+        return data;
+
     } catch (error) {
         console.error("Error fetching users: ", error);
         throw error;
