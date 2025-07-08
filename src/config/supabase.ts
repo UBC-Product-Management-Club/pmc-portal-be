48bbad02ebe8324b84389c7a6e2af29e860ce9b2
Kevin Lei <kevinleimc@gmail.com>
2025-07-07 18:51:06 -0700

    removed hook

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,19 +1,10 @@
 import dotenv from "dotenv"
 import { createClient, SupabaseClient } from '@supabase/supabase-js'
-import { useEnvironment } from "../utils/useEnvironment";
 
 dotenv.config({ path: "./.secret/.env" });
-const { isProd } = useEnvironment();
 
-const supabaseUrl =
-    isProd
-      ? process.env.PROD_SUPABASE_URL
-      : process.env.STAGING_SUPABASE_URL;
-
-const supabaseKey = 
-    isProd
-      ? process.env.PROD_SUPABASE_SERVICE_ROLE_KEY
-      : process.env.STAGING_SUPABASE_SERVICE_ROLE_KEY;
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY
 
 if (!supabaseUrl || !supabaseKey) {
     throw new Error(
