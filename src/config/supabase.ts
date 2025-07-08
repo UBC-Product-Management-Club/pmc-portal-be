208d6507c14017c4c781d9153298b35bf96c9426
Kevin Lei <81483109+kevsoupss@users.noreply.github.com>
2025-07-08 14:13:53 -0700

    setup supabase client (#72)
    * setup supabase client
    
    * removed hook
    
    * updated workflows

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,11 +1,17 @@
 import dotenv from "dotenv"
-import { createClient } from '@supabase/supabase-js'
+import { createClient, SupabaseClient } from '@supabase/supabase-js'
 
 dotenv.config({ path: "./.secret/.env" });
 
-console.log("Database URL: ", process.env.SUPABASE_URL!);
-console.log("Key", process.env.SUPABASE_ANON_KEY!);
-// Create a single supabase client for interacting with your database
-const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!);
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY
+
+if (!supabaseUrl || !supabaseKey) {
+    throw new Error(
+      "Missing required Supabase environment variables"
+    );
+  }
+
+const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
 
 export { supabase }
