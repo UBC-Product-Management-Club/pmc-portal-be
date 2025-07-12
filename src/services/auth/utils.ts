0ca6e783de31454e38ecb8978d24ed777754c378
Kevin Lei <kevinleimc@gmail.com>
2025-07-12 16:20:07 -0700

    fixed some folder path stuff

--- src/services/auth/utils.ts
+++ src/services/auth/utils.ts
@@ -1,5 +1,5 @@
 import { Parser } from "@json2csv/plainjs";
-import { exportUserFieldNames, UserExportFields } from "../../schema/User";
+import { exportUserFieldNames, UserExportFields } from "../../schema/v1/User";
 
 const formatCSV = (users: UserExportFields[]) => {
     const parser = new Parser({
