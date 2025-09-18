c398cab724a252c5097d878c5f2373a1cd13c395
Toby <toby.lau075@gmail.com>
2025-09-17 22:44:55 -0700

    fix

--- src/routes/v2/stripe.ts
+++ src/routes/v2/stripe.ts
@@ -8,8 +8,7 @@ export const webhookRouter = Router();
 webhookRouter.post("/", express.raw({ type: "application/json" }), async (req, res) => {
     try {
         const sig = req.headers["stripe-signature"] as string;
-        // const event = stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_WEBHOOK_SECRET!);
-        const event = stripe.webhooks.constructEvent(req.body, sig, 'whsec_26e2989099d5fa3a872f71edab23970d66d98da25227f3734bcf91ab459b04a1');
+        const event = stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_WEBHOOK_SECRET!);
 
         // responds 200 to stripe immediately
         res.status(200).send();
