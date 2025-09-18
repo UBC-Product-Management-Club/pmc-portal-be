e401a1cdf41ca39c7cc2e42fb5f8a8190c781528
Kevin Lei <81483109+kevsoupss@users.noreply.github.com>
2025-09-17 22:33:50 -0700

    added supabase auth token middleware

--- src/middleware/Session.ts
+++ src/middleware/Session.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { auth } from "express-oauth2-jwt-bearer";
 import { getUser } from "../services/User/UserService";
+import { supabase } from "../config/supabase";
 
 export const jwtCheck = auth({
     audience: process.env.JWT_AUDIENCE ?? "http://localhost:8000",
@@ -25,6 +26,22 @@ export const sessionFilter = async (req: Request, res: Response, next: NextFunct
         throw err;
     }
     next();
+};
+
+export const supabaseJwtCheck = async (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+  console.log(authHeader)
+  if (!authHeader) {
+    return res.status(401).json({ message: "Missing Authorization header" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  const { data, error } = await supabase.auth.getUser(token);
+  if (error || !data?.user) {
+    return res.status(401).json({ message: "Invalid Supabase token" });
+  }
+  next();
 };
 
 export const authenticated = [jwtCheck, sessionFilter];
