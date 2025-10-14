1f502e3c8b51b0546397ccc5f19077b95c79445a
Toby <toby.lau075@gmail.com>
2025-10-14 16:56:21 -0700

    refactor admin route (#130)
    * refactor admin route
    
    * fix typo

--- src/routes/v2/admin/event.ts
+++ src/routes/v2/admin/event.ts
@@ -1,71 +1,26 @@
 import { Request, Response, Router } from "express";
-import { EventSchema, EventInsert } from "../../schema/v2/Event";
-import { uploadSupabaseFiles } from "../../storage/Storage";
-import { v4 as uuidv4 } from 'uuid';
+import { uuidv4 } from "zod/v4";
+import { supabase } from "../../../config/supabase";
+import { EventSchema } from "../../../schema/v2/Event";
+import { addEvent } from "../../../services/Event/EventService";
+import { uploadSupabaseFiles } from "../../../storage/Storage";
 import multer from "multer"
-import { exportUsers, getUser, getUsers } from "../../services/User/UserService";
-import { addEvent } from "../../services/Event/EventService";
-
-export const adminRouter = Router();
 
 const memStorage = multer.memoryStorage()
 const upload = multer({ storage: memStorage })
+export const eventRouter = Router()
 
-adminRouter.get("/users", async (req: Request, res: Response) => {
-    try {
-        const users = await getUsers();
-        return res.status(200).send(users);
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send(error);
+eventRouter.get("/basic", async (req: Request, res: Response) => {
+    const {data, error} = await supabase.from('Event').select('event_id, name')
+    if (error || !data) {
+        console.error("Error fetching events: ", error);
+        return res.status(500).json(error)
     }
-});
-
-adminRouter.get("/users/export", async (req: Request, res: Response) => {
-    try {
-        const password = req.query.password as string;
-
-        if (!password) {
-            return res.status(401).send({ error: "Password is required" });
-        }
-
-        const isCSV = req.query.csv === "true";
-        const users = await exportUsers(password, isCSV);
-
-        if (isCSV) {
-            res.setHeader("Content-Type", "text/csv");
-            res.setHeader("Content-Disposition", "attachment; filename=users.csv");
-            return res.status(200).send(users);
-        }
-
-        res.setHeader("Content-Type", "application/json");
-        res.setHeader("Content-Disposition", "attachment; filename=users.json");
-        return res.status(200).json(users);
-    } catch (error: any) {
-        console.error(error);
-        if (error.message === "Invalid password") {
-            return res.status(401).send({ error: "Invalid password" });
-        }
-        return res.status(500).send({ error: "Failed to export users" });
-    }
-});
-
-adminRouter.get("/users/:id", async (req: Request, res: Response) => {
-    try {
-        const user = await getUser(req.params.id);
-        if (user) {
-            return res.status(200).json(user);
-        } else {
-            return res.status(404).json({ error: "User not found" });
-        }
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: "Failed to get user" });
-    }
-});
+    return res.status(200).json(data)
+})
 
 //[WIP Needs to be retested]
-adminRouter.post('/events/add', upload.fields([{ name: 'mediaFiles', maxCount: 5 },{ name: 'thumbnail', maxCount: 1 }]), async (req, res) => {
+eventRouter.post('/add', upload.fields([{ name: 'mediaFiles', maxCount: 5 },{ name: 'thumbnail', maxCount: 1 }]), async (req, res) => {
     const event_Id = uuidv4();
 
     // Unpacking request body
