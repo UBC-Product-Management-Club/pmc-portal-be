0e4bc2575a931aabfb8efb5ed9173851672533fa
Toby <toby.lau075@gmail.com>
2025-10-31 18:57:59 +0800

    supabase scripts

---
+++ scripts/gen_types.sh
@@ -0,0 +1,7 @@
+source .secret/.env
+FILE="src/schema/v2/database.types.ts"
+if [ -f "$FILE" ]; then
+    rm "$FILE"
+fi
+npx supabase gen types typescript --project-id "$SUPABASE_PROJECT_ID" --schema public > src/schema/v2/database.types.ts
+echo "Database types file created"
