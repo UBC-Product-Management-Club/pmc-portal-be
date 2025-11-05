dd6176cd49a13514bb6f3d0836ba08f4dd129f51
Toby <toby.lau075@gmail.com>
2025-11-05 19:53:17 +0700

    im gonna crashout

--- tests/setup.ts
+++ tests/setup.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config({path: './.secret/.env'});
 
+jest.mock("../src/config/supabase")
 jest.mock('../src/storage/UserRepository');
 jest.mock('../src/storage/AttendeeRepository');
 jest.mock('../src/storage/EventRepository');
