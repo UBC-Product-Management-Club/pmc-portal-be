4219844426e4e3f86fbfd79fd2b9fef04c37d52b
Toby <toby.lau075@gmail.com>
2026-02-19 20:49:49 -0800

    fix webhook

--- src/routes/v2/stripe.ts
+++ src/routes/v2/stripe.ts
@@ -5,18 +5,25 @@ import { handleStripeEvent } from "../../services/Payment/PaymentService";
 
 export const webhookRouter = Router();
 
-webhookRouter.post("/", express.raw({ type: "application/json" }), async (req, res) => {
+webhookRouter.post(
+  "/",
+  express.raw({ type: "application/json" }),
+  async (req, res) => {
     try {
-        const sig = req.headers["stripe-signature"] as string;
-        // const event = stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_WEBHOOK_SECRET!);
-        const event = stripe.webhooks.constructEvent(req.body, sig, "whsec_26e2989099d5fa3a872f71edab23970d66d98da25227f3734bcf91ab459b04a1");
+      const sig = req.headers["stripe-signature"] as string;
+      const event = stripe.webhooks.constructEvent(
+        req.body,
+        sig,
+        process.env.STRIPE_WEBHOOK_SECRET!
+      );
 
-        // responds 200 to stripe immediately
-        res.status(200).send();
+      // responds 200 to stripe immediately
+      res.status(200).send();
 
-        handleStripeEvent(event)
+      handleStripeEvent(event);
     } catch (error) {
-        console.error("Webhook Error:", error);
-        return res.status(400).send(`Webhook Error: ${error}`);
+      console.error("Webhook Error:", error);
+      return res.status(400).send(`Webhook Error: ${error}`);
     }
-});
+  }
+);
