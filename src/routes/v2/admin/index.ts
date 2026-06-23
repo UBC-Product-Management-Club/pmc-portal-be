1fa6d95162ef92681acf1d475474a47f03106865
Connor Young <connoryoung@Connors-MacBook-Air-2.local>
2026-06-22 20:43:15 -0400

    added endpoints and schema for recruit form

--- src/routes/v2/admin/index.ts
+++ src/routes/v2/admin/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { userRouter} from "./users";
 import { eventRouter } from "./event";
+import { recruitingFormRouter } from "./recruitingForm";
 
 export const adminRouter = Router();
 
 adminRouter.use("/users", userRouter)
 adminRouter.use("/events", eventRouter)
+adminRouter.use("/recruiting-forms", recruitingFormRouter)
 
